0c1ffd6fda05003a0f8625056a9621926487a05d
faisal-2d <abdullah71faisal@gmail.com>
2022-05-06 08:27:38 +0300

    api update item added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
@@ -21,37 +21,54 @@ async function run() {
     await client.connect();
     const warehouseCollection = client.db("warehousedb").collection("toys");
 
-
     // get all items
     // http://localhost:5000/toys
-    app.get('/toys', async (req,res) =>{
-        const query = {};
-        const result = await warehouseCollection.find(query).toArray();
-        res.send(result)
-    })
+    app.get("/toys", async (req, res) => {
+      const query = {};
+      const result = await warehouseCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    // find one item 
-    app.get('/item/:id', async (req,res) =>{
-        const id = req.params.id;
-        const filter = {_id: ObjectId(id)};
-        const result = await warehouseCollection.findOne(filter);
-        res.send(result)
-    })
-
+    // find one item
+    // http://localhost:5000/item/6274a3425a04790168facc8c
+    app.get("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await warehouseCollection.findOne(filter);
+      res.send(result);
+    });
 
     // create one item
     // http://localhost:5000/toys
-    app.post('/item', async (req, res)=>{
-        const item = req.body;
-        const result = await warehouseCollection.insertOne(item);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    res.send({message: "item added"})
-    })
+    app.post("/item", async (req, res) => {
+      const item = req.body;
+      const result = await warehouseCollection.insertOne(item);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.send({ message: "item added" });
+    });
 
     //update item
+    app.put("/item/:id", async (req, res) => {
+      const updatedData = req.body;
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const updateItem = {
+        $set: {
+          name : updatedData.name,
+          img: updatedData.img,
+          description: updatedData.description,
+          price: updatedData.price,
+          quantity: updatedData.quantity,
+          supplier: updatedData.supplier,
+          sold: updatedData.sold,
+        },
+      };
+      const result = await warehouseCollection.updateOne(filter, updateItem);
+
+      res.send({message: "item updated"})
+    });
 
     // delete item
-
   } finally {
   }
 }
