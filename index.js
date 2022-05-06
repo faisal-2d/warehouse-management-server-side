e65f9aea66c0e783642bedef9e4546e522d531fe
faisal-2d <abdullah71faisal@gmail.com>
2022-05-06 06:35:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT ||5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("welcome to warehouse")
+  });
+  
+  app.listen(port, () => {
+    console.log('listening port', port);
+  })
+  
